4fa2c039c98c4e8ec0bee7106e008ba206f33642
Arnelle Balane <arnellebalane@gmail.com>
2019-12-14 13:52:12 +0800

    Update code for Day 05

--- src/05/index.mjs
+++ src/05/index.mjs
@@ -1,7 +1,6 @@
-export const DEFAULT_INPUT = 5;
-
-export function evaluateProgram(program) {
+export function evaluateProgram(program, getInput=() => 0) {
   let i = 0;
+  let input = 0;
 
   while (i < program.length) {
     const instruction = program[i];
@@ -20,9 +19,11 @@ export function evaluateProgram(program) {
 
     if (opcode === 99) {
       break;
+    } else if (opcode === 3) {
+      input = getInput();
     }
 
-    [i, program] = opcodeHandlers[opcode](i, program);
+    [i, program] = opcodeHandlers[opcode](i, program, input);
   }
 
   return program;
@@ -38,46 +39,54 @@ function extractParameters(ptr, program) {
   return [a, b, output];
 }
 
+// Opcode 1: Addition
 function opcode1(ptr, program) {
   const [a, b, output] = extractParameters(ptr, program);
   program[output] = a + b;
   return [ptr + 4, program];
 }
 
+// Opcode 2: Multiplication
 function opcode2(ptr, program) {
   const [a, b, output] = extractParameters(ptr, program);
   program[output] = a * b;
   return [ptr + 4, program];
 }
 
-function opcode3(ptr, program) {
+// Opcode 3: Read Input
+function opcode3(ptr, program, input) {
   const output = program[ptr + 1];
-  program[output] = DEFAULT_INPUT;
+  program[output] = input;
   return [ptr + 2, program];
 }
 
+// Opcode 4: Write Output
 function opcode4(ptr, program) {
   const address = program[ptr + 1];
   program[0] = program[address];
   return [ptr + 2, program];
 }
 
+// Opcode 5: Jump If True
 function opcode5(ptr, program) {
   const [a, b] = extractParameters(ptr, program);
   return [a === 0 ? ptr + 3 : b, program];
 }
 
+// Opcode 6: Jump If False
 function opcode6(ptr, program) {
   const [a, b] = extractParameters(ptr, program);
   return [a === 0 ? b : ptr + 3, program];
 }
 
+// Opcode 7: Less Than
 function opcode7(ptr, program) {
   const [a, b, output] = extractParameters(ptr, program);
   program[output] = a < b ? 1 : 0;
   return [ptr + 4, program];
 }
 
+// Opcode 8: Equals
 function opcode8(ptr, program) {
   const [a, b, output] = extractParameters(ptr, program);
   program[output] = a === b ? 1 : 0;
@@ -90,6 +99,6 @@ function opcode8(ptr, program) {
 import program from './input.mjs';
 
 if (process.env.NODE_ENV !== 'test') {
-  const output = evaluateProgram(program);
+  const output = evaluateProgram(program, () => 5);
   console.log(output[0]);
 }
